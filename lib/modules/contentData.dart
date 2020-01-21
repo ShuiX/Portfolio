3f621ec15753c8c97f8ee9113bfd8b022e825f3c
ShuiX <shajith.307@gmail.com>
2020-01-21 13:22:54 +0100

    added content

--- lib/modules/contentData.dart
+++ lib/modules/contentData.dart
@@ -2,10 +2,10 @@ class ContentData {
   ContentData._();
 
   static List<String> contentText = [
-    '''I was born and brought up in a small and peaceful Town called \"Baar\". It is a town with many beautiful sights and very well-mannered people. It may not be one of the well known places out there, but it's 
+    '''I was born and brought up in a small and peaceful town called \"Baar\". It is a town with many beautiful sights and very well-mannered people. It may not be one of the well known places out there, but it's worth a visit for anyone. And thanks to it's kind and beautiful neighborhood there, i have brought up good qualities in my life which represents a part of myself and my town.
 
-Thanks to it's good atmosphere there, i have brought with me these good qualities to represent which i am really proud of.''',
-    '''SchlussSchlussSchlussSchlussSchlussSchlussSchlussSchlussSchlussSchluss''',
+My passion for technology already began when i was a kid. Even though it was the worst case scenario for my parents, because of the tremendous damage i've caused, through these trials and errors i've built my way up here where i am now''',
+    '''As time went on, i began to get more into IT Science. The internet was starting to become a mainstream thing bringing with itself new adventures. Like the adventurous kid i was back then, i went along the ride and grew up with the \"internet cult\". I could say that i received and made alot of contributions on the internet, be it on social media, video portals or online forums.''',
   ];
 
   static List<String> bioListText = [
