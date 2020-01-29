741704e5f4246f39089da1e4bc12cbba49f4bb9e
ShuiX <shajith.307@gmail.com>
2020-01-29 12:02:41 +0100

    Progress on Project page

--- lib/pages/myProjects.dart
+++ lib/pages/myProjects.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/modules/LinearHalfCurve.dart';
 import 'package:portfolio/modules/animatedWindow.dart';
 import 'package:portfolio/modules/responsiveScreen.dart';
 
@@ -10,6 +11,8 @@ class Projects extends StatefulWidget {
 class _ProjectsState extends State<Projects> {
   Alignment _alignment = Alignment.center;
   double _padding = 0;
+  double _opacity = 0;
+  Widget _animatedWidget;
 
   @override
   void initState() {
@@ -22,8 +25,14 @@ class _ProjectsState extends State<Projects> {
       setState(() {
         this._alignment = Alignment.topCenter;
         this._padding = 55;
+        this._opacity = 1;
       });
     });
+  }
+
+  Widget _projectSurvey(
+      BuildContext context, ResponsiveScreen responsiveScreen) {
+    return Container();
   }
 
   Widget _content(BuildContext context, ResponsiveScreen responsiveScreen) {
@@ -40,6 +49,23 @@ class _ProjectsState extends State<Projects> {
                 .textTheme
                 .headline6
                 .copyWith(fontSize: responsiveScreen.titleSize),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: AnimatedOpacity(
+            opacity: this._opacity,
+            duration: Duration(milliseconds: 1550),
+            curve: LinearHalfCurve().flipped,
+            child: AnimatedSwitcher(
+              duration: Duration(seconds: 1),
+              transitionBuilder: (Widget child, Animation<double> animation) =>
+                  RotationTransition(
+                child: child,
+                turns: animation,
+              ),
+              child: _animatedWidget,
+            ),
           ),
         ),
       ],
