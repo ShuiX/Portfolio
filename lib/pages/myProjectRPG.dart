d73d2362ac01bbf49ec11fd14987bbfc110d034d
ShuiX <shajith.307@gmail.com>
2020-02-24 13:51:33 +0100

    Content Update for ProjectPage

--- lib/pages/myProjectRPG.dart
+++ lib/pages/myProjectRPG.dart
@@ -18,41 +18,115 @@ class ProjectRPG extends StatelessWidget {
             ),
           ),
           FractionallySizedBox(
-            heightFactor: responsiveScreen.heightFactor,
-            widthFactor: responsiveScreen.widthFactor,
-            child: Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(top: 55),
-              child: RichText(
-                text: TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: "Project",
-                      style: TextStyle(
-                        fontSize: responsiveScreen.titleSize,
-                        color: Colors.white,
-                        fontFamily: "Terminal",
-                      ),
+            heightFactor: 1.0,
+            widthFactor: 1.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topCenter,
+                  padding: EdgeInsets.only(top: 55),
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: " Project",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize,
+                            color: Colors.white,
+                            fontFamily: "Terminal",
+                            shadows: [
+                              Shadow(
+                                color: Colors.white.withOpacity(1.0),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextSpan(
+                          text: "_",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize,
+                            color: Colors.blue,
+                            fontFamily: "Fantasy",
+                            shadows: [
+                              Shadow(
+                                color: Colors.blue.withOpacity(1.0),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Darzana ",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize,
+                            color: Colors.pink,
+                            fontFamily: "Elven",
+                            shadows: [
+                              Shadow(
+                                color: Colors.pink.withOpacity(1.0),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    TextSpan(
-                      text: "_",
-                      style: TextStyle(
-                        fontSize: responsiveScreen.titleSize,
-                        color: Colors.black,
-                        fontFamily: "Fantasy",
-                      ),
-                    ),
-                    TextSpan(
-                      text: "Darzana",
-                      style: TextStyle(
-                        fontSize: responsiveScreen.titleSize,
-                        color: Colors.pink,
-                        fontFamily: "Elven",
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-              ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  alignment: Alignment.centerRight,
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "A Fantasy RPG With \nStorytelling Adventure",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize * 0.8,
+                            color: Colors.white,
+                            fontFamily: "Fantasy",
+                            shadows: [
+                              Shadow(
+                                color: Colors.white.withOpacity(1.0),
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextSpan(
+                          text: "\n> ",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize * 0.6,
+                            color: Colors.blue,
+                            fontFamily: "Fantasy",
+                            shadows: [
+                              Shadow(
+                                color: Colors.blue.withOpacity(1.0),
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextSpan(
+                          text: "In Development",
+                          style: TextStyle(
+                            fontSize: responsiveScreen.titleSize * 0.8,
+                            color: Colors.pink,
+                            fontFamily: "Fantasy",
+                            shadows: [
+                              Shadow(
+                                color: Colors.pink.withOpacity(1.0),
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
