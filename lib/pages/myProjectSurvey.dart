2a26085ca1a8a73af023b1ac7a53bc050e153b39
ShuiX <shajith.307@gmail.com>
2020-03-02 12:25:20 +0100

    added content to the SurveyProjectPage

--- lib/pages/myProjectSurvey.dart
+++ lib/pages/myProjectSurvey.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/modules/responsiveScreen.dart';
 
+import 'dart:js' as js;
+
 class ProjectSurvey extends StatelessWidget {
   ProjectSurvey();
 
-  Widget _content(BuildContext context, ResponsiveScreen responsiveScreen) {
+  Widget _content(BuildContext context, ResponsiveScreen responsiveScreen,
+      double iconSize) {
     return Card(
       child: FractionallySizedBox(
         heightFactor: responsiveScreen.heightFactor,
@@ -15,11 +18,52 @@ class ProjectSurvey extends StatelessWidget {
               alignment: Alignment.topCenter,
               padding: EdgeInsets.only(top: 55),
               child: Text(
-                "FlowSurvey",
+                "FlowSurvey\n",
                 style: Theme.of(context).textTheme.headline6.copyWith(
                       fontSize: responsiveScreen.titleSize,
                       fontFamily: "BlackChancery",
                       color: Colors.white,
+                    ),
+              ),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Text(
+                      "Survey Done Easy!\nClick on the Arrow for a Demo ",
+                      style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            fontSize: responsiveScreen.subTitleSize,
+                            color: Colors.white,
+                            fontFamily: "BlackChancery",
+                          ),
+                    ),
+                  ),
+                  Flexible(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed: () => js.context.callMethod(
+                        "open",
+                        ["https://flowsurvey-2020.web.app/#/startDemo"],
+                      ),
+                      iconSize: iconSize,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.only(bottom: 75, left: 20, right: 20),
+              child: Text(
+                "//FlowSurvey is still a Work in Progress application and may include some bugs",
+                style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      fontSize: responsiveScreen.subTitleSize * 0.75,
+                      color: Colors.white,
+                      fontFamily: "BlackChancery",
                     ),
               ),
             ),
@@ -37,11 +81,11 @@ class ProjectSurvey extends StatelessWidget {
         if (constraint.maxWidth < 840) {
           responsiveScreen = ResponsiveScreen(
               titleSize: 30, subTitleSize: 20, widthFactor: 1, heightFactor: 1);
-          return this._content(context, responsiveScreen);
+          return this._content(context, responsiveScreen, 50);
         } else {
           responsiveScreen = ResponsiveScreen(
-              titleSize: 60, subTitleSize: 30, widthFactor: 1, heightFactor: 1);
-          return this._content(context, responsiveScreen);
+              titleSize: 65, subTitleSize: 30, widthFactor: 1, heightFactor: 1);
+          return this._content(context, responsiveScreen, 100);
         }
       },
     );
